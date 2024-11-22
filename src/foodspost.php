1c54f2f4f04c878bab895f7cf1f95e76daad2feb
Yoshiki_Kato <it232075@trident.ac.jp>
2024-11-22 12:52:35 +0900

    アップロード仮

--- src/foodspost.php
+++ src/foodspost.php
@@ -7,14 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'] ?? '';
     $location = $_POST['location'] ?? '';
     $amount = $_POST['amount'] ?? '';
-    $place = $_POST['place'] ?? '';
+    $price = $_POST['price'] ?? '';
     $expiration_date = $_POST['expiration_date'] ?? '';
     $email = $_POST['email'] ?? '';
+    $post_date = date('Y-m-d');
 
     // データベースに保存
     if ($name && $location && $amount && $place && $expiration_date && $email) { // 必須フィールドを確認
         $stmt = $db->prepare('INSERT INTO posts (name, location, amount, place, expiration_date, email) VALUES (?, ?, ?, ?, ?, ?)');
-        $stmt->bind_param('ssss', $name, $place, $amount, $email); // 型指定 s=文字列
+        $stmt->bind_param('ssssss', $name, $location, $amount, $place, $expiration, $email); // 型指定 s=文字列
 
         if ($stmt->execute()) {
             // データ保存成功後に画面遷移
@@ -26,5 +27,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo '全てのフィールドを入力してください。';
     }
+    //アップロード先ディレクトリ
+    //$uploadDir = 'postimg/';
+
+    //アップロードされたファイル情報
+    //$file = $_FILES['file'];
+
 }
 ?>
