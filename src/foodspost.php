31edefe48d8cc81e5fe9040c3126a20110a65d0b
Yoshiki_Kato <it232075@trident.ac.jp>
2024-11-22 11:33:05 +0900

    投稿画面

--- src/foodspost.php
+++ src/foodspost.php
@@ -1,27 +1,30 @@
-<?php require('db/dbconnect-w.php'); ?>
-
 <?php
+require('db/dbconnect-w.php'); // DB接続ファイル
 session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // POSTデータを取得
+    $name = $_POST['name'] ?? '';
+    $location = $_POST['location'] ?? '';
+    $amount = $_POST['amount'] ?? '';
+    $place = $_POST['place'] ?? '';
+    $expiration_date = $_POST['expiration_date'] ?? '';
+    $email = $_POST['email'] ?? '';
+
+    // データベースに保存
+    if ($name && $location && $amount && $place && $expiration_date && $email) { // 必須フィールドを確認
+        $stmt = $db->prepare('INSERT INTO posts (name, location, amount, place, expiration_date, email) VALUES (?, ?, ?, ?, ?, ?)');
+        $stmt->bind_param('ssss', $name, $place, $amount, $email); // 型指定 s=文字列
+
+        if ($stmt->execute()) {
+            // データ保存成功後に画面遷移
+            header('Location: index.php');
+            exit;
+        } else {
+            echo 'データ保存に失敗しました: ' . $stmt->error;
+        }
+    } else {
+        echo '全てのフィールドを入力してください。';
+    }
+}
 ?>
-    <head>
-        <meta charset = "UTF-8">
-        <link rel="stylesheet" href="css/master.css">
-    </head>
-
-    <body>
-        <form action="b.php" method="POST">
-            画像  <input type="file" name="file"><br/>
-            名前 <br/> <input type="text" name="id"><br/>
-            場所 <br/> <input type="text" name="id"><br/>
-            量　 <br/> <input type="text" name="id"><br/>
-            メールアドレス <br/> <input type="email" name="mail"><br/>
-            <input type="submit" value="投稿" >
-
-<!-- 
-    ・ 投稿おしたら、メイン画面に戻るプログラム
-                　MySqlのテーブルにデータを保存させるプログラム
-    ・
--->
-
-        </form>
-    </body>
