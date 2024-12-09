bb998afe829a3b94c7dee7654f8a0d8595fed7f0
Yoshiki_Kato <it232075@trident.ac.jp>
2024-12-09 16:45:40 +0900

    データベースの保存成功

--- src/foodspost.php
+++ src/foodspost.php
@@ -4,29 +4,37 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // POSTデータを取得
+    $url = $_POST['url'] ?? '';
     $name = $_POST['name'] ?? '';
     $location = $_POST['location'] ?? '';
     $amount = $_POST['amount'] ?? '';
     $price = $_POST['price'] ?? '';
     $expiration_date = $_POST['expiration_date'] ?? '';
     $email = $_POST['email'] ?? '';
-    $post_date = date('YYYY-MM-DD HH:MM:SS');
+    $post_date = date( 'YYYY-MM-DD HH:MM:SS');
+
 
     // データベースに保存
-    if ($name && $location && $amount && $price && $expiration_date && $email) { // 必須フィールドを確認
-        $stmt = $dbw->prepare('INSERT INTO posts (name, location, amount, place, expiration_date, email) VALUES (?, ?, ?, ?, ?, ?)');
-        $stmt->bind_param('ssssss', $name, $location, $amount, $price, $expiration, $email); // 型指定 s=文字列
+    // if(!empty($name) && !empty($location) && !empty($amount) && !empty($price) &&!empty($expiration_date) && !empty($email) && !empty($url)) {// 必須フィールドを確認
+
+    if(!empty($name)){
+        $stmt = $dbw->prepare('INSERT INTO posts (name, location, amount, price, expiration_date, email, img_url) VALUES ("'.$name.'","'.$location.'",'.$amount.','.$price.',"'.$expiration_date.'","'.$email.'","'.$url.'")' );
+       echo 'INSERT INTO posts (name, location, amount, price, expiration_date, email, url) VALUES ("'.$name.'","'.$location.'",'.$amount.','.$price.',"'.$expiration_date.'","'.$email.'","'.$url.'")'; 
 
         if ($stmt->execute()) {
             // データ保存成功後に画面遷移
-            header('Location: index.php');
+            header('Location: ./index.php');
             exit;
         } else {
-            echo 'データ保存に失敗しました: ' . $stmt->error;
+             echo 'データ保存に失敗しました: ' ; $stmt->error;
         }
     } else {
         echo '全てのフィールドを入力してください。';
+
     }
+
+    
+    
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
